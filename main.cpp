12e9e90e8c9bd5e09df9dfe53ea7ccdd6e7c7a89
mbabunypl <mahamudbabu@nypl.org>
2024-01-09 13:38:19 -0500

    test message

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
 using namespace std;
+
+
+void square(int x){
+    
+}
 int main(){
   int x = 3;
   int y = 42;
   std::cout << x+y << std::endl;
   cout << "Hello Wrld" << std::endl;
   std::cout << x << std::endl;
-  int square(3);
 
  
 }
